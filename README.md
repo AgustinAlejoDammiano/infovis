89a0367a7021bbaecc552776139d23aa74ec4a09
agustin <adammiano@itba.edu.ar>
2021-03-13 09:46:33 -0300

    improve gh-pages

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # infovis
 
+[gh-pages](https://AgustinAlejoDammiano.github.io/infovis/)
+
 [Cheatsheet Markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
---
+++ js/main.js
@@ -0,0 +1,14 @@
+const url_prefix = "pages/"
+let makeovers = [
+    {'title': "Makeover Mondays - Week 9", "url": "makeoverMondayWeek9.html"},
+]
+
+d3.select("#pages-listt")
+    .selectAll("li")
+    .data(makeovers)
+    .enter()
+    .append("li")
+    .append("a")
+    .text(d => d.title)
+    .attr("href", d => url_prefix + d.url);
+    
