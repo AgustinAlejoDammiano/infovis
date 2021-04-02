0b3b0e49ff1b821dd6afd107dc9fba1933665d70
AgustinAlejoDammiano <adammiano@itba.edu.ar>
2021-04-02 16:34:20 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const url_prefix = "pages/"
 let pages = [
     {'title': "Makeover Mondays - Week 9", "url": "makeoverMondayWeek9.html"},
     {'title': "Makeover Mondays - Week 11", "url": "makeoverMondayWeek11.html"},
+    {'title': "Argentina Población", "url": "argentinaPoblacion.html"},
 ]
 
 d3.select("#pages-list")
