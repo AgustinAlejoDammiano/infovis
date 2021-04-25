aa0c5fca75b02dc027d76c8121a9fa4a08e3ff47
AgustinAlejoDammiano <adammiano@itba.edu.ar>
2021-04-25 12:01:27 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let pages = [
     {'title': "Makeover Mondays - Week 9", "url": "makeoverMondayWeek9.html"},
     {'title': "Makeover Mondays - Week 11", "url": "makeoverMondayWeek11.html"},
     {'title': "Argentina Población", "url": "argentinaPoblacion.html"},
+    {'title': "TP Personal Data", "url": "TPPersonalData.html"},
 ]
 
 d3.select("#pages-list")
